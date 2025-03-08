4004aa3e0b7fd4d4ebb4befc41e8da68205bebfd
jangker75 <jangker_75@ymail.com>
2025-03-08 14:26:22 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
   const [jadwal, setJadwal] = useState<Jadwal | null>(null)
   const [isTimeSolat, setIsTimeSolat] = useState(false)
   const [solatNow, setSolatNow] = useState("")
+  const [solatNext, setSolatNext] = useState("")
+  const [timecountdown, setTimecountdown] = useState("")
   const urlJadwal = "https://api.myquran.com/v2/sholat/jadwal/1301/"
   const hours = time.getHours();
   const minutes = time.getMinutes();
@@ -31,8 +33,7 @@ function App() {
       settime(new Date());
       setBlink(!blink);
       checkTimeSolat()
-      console.log(isTimeSolat);
-      
+      countdownsolat()
     }, 1000);
     return () => clearInterval(intervalId);
   }, [blink]);
@@ -42,38 +43,61 @@ function App() {
 
   const getJadwalAzan = async () => {
     const dd = new Date()
-
-    const response = await fetch(urlJadwal + year + "/" + padZero(dd.getMonth() + 1) + "/" + padZero(dd.getDate()));
-    const data = await response.json();
-    const tmpdate = data.data.jadwal.tanggal
-    setDate(tmpdate)
-    const tmpcity = data.data.daerah
-    setCity(tmpcity)
-    const tmpjadwal: Jadwal = data.data.jadwal as Jadwal
-    setJadwal(tmpjadwal)
+    const url = `${urlJadwal}${year}/${padZero(dd.getMonth() + 1)}/${padZero(dd.getDate())}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setDate(data.data.jadwal.tanggal);
+      setCity(data.data.daerah);
+      setJadwal(data.data.jadwal as Jadwal);
+    } catch (error) {
+      console.error(error);
+    }
   }
   const checkTimeSolat = () => {
     if (jadwal) {
+      const nowTimeInMinutes = hours * 60 + minutes;
+      const jedanotifsolat = 5;
+
       for (const [key, value] of Object.entries(jadwal)) {
-        const startHour = parseInt(value.split(":")[0]);
-        const startMinute = parseInt(value.split(":")[1]);
-        const nowTimeInMinutes = hours * 60 + minutes;
+        const [startHour, startMinute] = value.split(":").map(Number);
         const startTimeInMinutes = startHour * 60 + startMinute;
-        const endTimeInMinutes = startHour * 60 + startMinute + 5;
+        const endTimeInMinutes = startHour * 60 + startMinute + jedanotifsolat;
+
         if (nowTimeInMinutes >= startTimeInMinutes && nowTimeInMinutes <= endTimeInMinutes) {
           setIsTimeSolat(true)
           setSolatNow(key)
           return
-        } else {
-          setIsTimeSolat(false)
-          setSolatNow("")
         }
+        setIsTimeSolat(false)
+        setSolatNow("")
       }
     }
   }
-  function capitalize(s: string)
-  {
-      return s && String(s[0]).toUpperCase() + String(s).slice(1);
+  const countdownsolat = () => {
+    // const dummy = "13:55"
+    if (jadwal) {
+      const aa: Jadwal = { ...jadwal }
+      // aa.ashar = dummy
+      // console.log("aa", aa);
+      const notifselisih = 5 * 60
+      const nowTimeInSeconds = hours * 3600 + (minutes * 60) + seconds;
+      for (const [key, value] of Object.entries(aa)) {
+        const [startHour, startMinute] = value.split(":").map(Number);
+        const startTimeInSeconds = startHour * 3600 + (startMinute * 60);
+        const selisih = startTimeInSeconds - nowTimeInSeconds
+        if (selisih > 0 && selisih <= notifselisih) {
+          setTimecountdown(convertDetikKeJam(selisih))
+          setSolatNext(key)
+          return
+        }
+        setTimecountdown("")
+        setSolatNext("")
+      }
+    }
+  }
+  function capitalize(s: string) {
+    return s && String(s[0]).toUpperCase() + String(s).slice(1);
   }
   const BlinkComponent = () => {
     return (
@@ -83,17 +107,24 @@ function App() {
   const ItemSolat = (title: string, time?: string) => {
     return (
       <div className='flex flex-row text-2xl'>
-        <p className='w-20'>{capitalize(title)}</p>
+        <p className='w-24 md:w-20'>{capitalize(title)}</p>
         <p className='mx-5'>:</p>
-        <p>{time}</p>
+        <p className='text-2xl font-bold'>{time}</p>
       </div>
     )
   }
+  function convertDetikKeJam(detik: number) {
+    // const jam = Math.floor(detik / 3600);
+    const menit = Math.floor((detik % 3600) / 60);
+    const detikSisa = detik % 60;
+
+    return `${menit.toString().padStart(2, '0')}:${detikSisa.toString().padStart(2, '0')}`;
+  }
   return (
     <>
-      <div className='h-screen bg-black'>
+      <div className='h-screen bg-black zain-regular'>
         <div className='absolute top-5 left-5 text-white'>
-          <p className='text-3xl'>Jadwal Solat Wilayah <span className='text-4xl'>{city}</span></p>
+          <p className='text-3xl zain-regular'>Jadwal Solat Wilayah <span className='text-4xl zain-bold'>{city}</span></p>
           {
             listSolat.map((item, index) => {
               return (
@@ -107,16 +138,22 @@ function App() {
 
         </div>
         <div className='flex justify-center items-center h-full flex-col'>
-          <p className='text-5xl text-blue-200'>
+          <p className='text-4xl md:text-5xl text-blue-200'>
             {date}
           </p>
-          <p className='text-9xl text-white font-bold'>
+          <p className='text-6xl md:text-9xl text-white zain-bold'>
             {padZero(hours)}<BlinkComponent />{padZero(minutes)}<BlinkComponent />{padZero(seconds)}
           </p>
           {
             isTimeSolat &&
-            <p className='text-6xl mt-4 animation-color-transition'>
+            <p className='text-4xl md:text-6xl text-center mt-4 animation-color-transition'>
               Waktu solat {capitalize(solatNow)} telah tiba
+            </p>
+          }
+          {
+            timecountdown &&
+            <p className='text-4xl md:text-6xl text-center mt-4 animation-color-transition'>
+              Waktu menuju solat<br />{capitalize(solatNext)} {timecountdown}
             </p>
           }
         </div>
